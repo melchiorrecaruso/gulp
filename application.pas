12c08c2effa3a09aeee998956d3a6f4aee84def2
Melchiorre Caruso <melchiorrecaruso@users.noreply.github.com>
2015-01-01 01:44:36 +0100

    Some changes.

--- application.pas
+++ application.pas
@@ -1,5 +1,5 @@
 {
-  Copyright (c) 2014 Melchiorre Caruso.
+  Copyright (c) 2014-2015 Melchiorre Caruso.
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
