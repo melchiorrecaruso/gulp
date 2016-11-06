5dffbe76bb9705df8bc9d86828a112c2ee7249fc
Melchiorre Caruso <melchiorrecaruso@users.noreply.github.com>
2016-11-06 10:07:01 +0100

    .

---
+++ gulp.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo "*** BUILD GULP DEB PACKCAGE ***"
+
+mkdir build
+
+lazbuild -B -q -q gulp.lpi
+lazbuild -B -q -q gulpx.lpi
+lazbuild -B -q -q gdiff.lpi
+
+gzip -9 -k -n install/gulp.man
+
+mv build/x86_64-linux/bin/gulp  build/gulp
+mv build/x86_64-linux/bin/gulpx build/gulpx
+mv build/x86_64-linux/bin/gdiff build/gdiff
+mv install/gulp.man.gz          build/gulp.1.gz
+cp install/CHANGELOG            build/CHANGELOG 
+cp install/LICENSE              build/LICENSE 
+cp install/README               build/README 
+cp install/gulp.equivs          build/gulp.equivs 
+
+cd build
+
+equivs-build gulp.equivs
+
+cd ..
+
+mv build/gulp_0.4_amd64.deb     releases/test_gulp_0.4_amd64.deb
+
+rm -f -r build
+
+echo "*** END ***"
+
+
+
+
